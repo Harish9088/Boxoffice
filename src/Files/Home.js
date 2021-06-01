6f853a324075328f953d762f7da0163a8901f773
harish90 <harishlycan9088@gmail.com>
2021-06-01 20:53:08 +0530

    Created radio buttons to obtain results of shows and actors

--- src/Files/Home.js
+++ src/Files/Home.js
@@ -6,6 +6,9 @@ import { apiGet } from "../misc/config"
 function Home() {
     const [input,setInput]=useState("")
     const [results,setResults]=useState(null)
+    const [searchOption,setSearchOption]=useState("shows")
+
+    const isSearchOption=searchOption==="shows"
 
     const onInputChange=(e)=>{
            setInput(e.target.value)
@@ -15,22 +18,33 @@ function Home() {
         onSearch()
  }
     const onSearch=()=>{
-       apiGet(`/search/shows?q=${input}`)
+       apiGet(`/search/${searchOption}?q=${input}`)
         .then(result=>
             {setResults(result);
             console.log(result)}
         )}
+
+      const onRadioChange=(e)=>{
+          setSearchOption(e.target.value)
+      }
+        
         const renderResults=()=>{
             if(results && results.length===0){
                 return <div>No data Found</div>
             }
             if(results && results.length>0){
-                return <div>{results.map(item=><div key={item.show.id}>{item.show.name}</div>)}</div>
+                return results[0].show?
+                results.map(item=><div key={item.show.id}>{item.show.name}</div>):
+                results.map(item=><div key={item.person.id}>{item.person.name}</div>)
             }
         }
     return (
         <Mainpages>
            <input type="text" value={input} onChange={onInputChange} onKeyDown={onKeyPress} placeholder="Search for movie or actor"/>
+           <div>
+               <label htmlFor="shows-search">Shows <input id="shows-search" onChange={onRadioChange} type="radio" value="shows" checked={isSearchOption}/></label>
+               <label htmlFor="actors-search">Actors <input id="actors-search" onChange={onRadioChange} type="radio" value="people" checked={!isSearchOption}/></label>
+           </div>
            <button onClick={onSearch}>Search</button>
           {renderResults()}
         </Mainpages>
